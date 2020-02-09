ec4f0521d0046e1fbfc2779ed5ec7f0f8a3d3f99
= <=>
2020-02-09 17:59:51 +0530

    updated sql

--- linkedin (1).sql
+++ linkedin (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2020 at 09:24 AM
+-- Generation Time: Feb 09, 2020 at 01:28 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -29,7 +29,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `admin_id` varchar(255) NOT NULL,
+  `admin_id` int(10) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -37,8 +38,8 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`admin_id`, `password`) VALUES
-('admin', '$2y$10$DnAKwyLtrX1JbMRVibwLUOLxENze0HhnSgE2iqNqWk4.refLW6rb6');
+INSERT INTO `admin` (`admin_id`, `username`, `password`) VALUES
+(2, 'admin', '$2y$10$CnBLNWWURGCrcmlI5fx8weDjwiV2d7hP0V/5yHYRkPn67R8WJt3qe');
 
 -- --------------------------------------------------------
 
@@ -47,7 +48,7 @@ INSERT INTO `admin` (`admin_id`, `password`) VALUES
 --
 
 CREATE TABLE `connection` (
-  `user_id` varchar(255) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `linked_id` varchar(255) NOT NULL,
   `followers` varchar(255) NOT NULL,
   `following` varchar(255) NOT NULL,
@@ -61,20 +62,22 @@ CREATE TABLE `connection` (
 --
 
 CREATE TABLE `news` (
-  `news_id` int(255) NOT NULL,
-  `news_title` varchar(255) NOT NULL,
+  `news_id` int(10) NOT NULL,
+  `news_title` varchar(100) NOT NULL,
   `startdate` date NOT NULL,
   `enddate` date NOT NULL,
-  `news_content` varchar(255) NOT NULL
+  `news_content` varchar(255) NOT NULL,
+  `news_image_path` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`news_id`, `news_title`, `startdate`, `enddate`, `news_content`) VALUES
-(1, 'sahgd', '2019-11-14', '2019-11-20', 'sdhsdgthjaestgaofigjh urhy gifj gpioasjfgpijfpig jipjipooifgiujebqeg'),
-(2, 'sahgd', '2019-11-14', '2019-11-20', 'sdhsdgthjaestgaofigjh urhy gifj gpioasjfgpijfpig jipjipooifgiujebqeg');
+INSERT INTO `news` (`news_id`, `news_title`, `startdate`, `enddate`, `news_content`, `news_image_path`) VALUES
+(1, 'sahgd', '2019-11-14', '2019-11-20', 'sdhsdgthjaestgaofigjh urhy gifj gpioasjfgpijfpig jipjipooifgiujebqeg', ''),
+(2, 'sahgd', '2019-11-14', '2019-11-20', 'sdhsdgthjaestgaofigjh urhy gifj gpioasjfgpijfpig jipjipooifgiujebqeg', ''),
+(3, 'sahgd', '2020-01-02', '2020-01-17', 'dszgldsfoghnsodfnbgouigdhg\r\n', '');
 
 -- --------------------------------------------------------
 
@@ -83,15 +86,23 @@ INSERT INTO `news` (`news_id`, `news_title`, `startdate`, `enddate`, `news_conte
 --
 
 CREATE TABLE `unconfirmed_signup` (
-  `Name` varchar(255) NOT NULL,
+  `Name` varchar(20) NOT NULL,
   `Enrollment` varchar(20) NOT NULL,
   `Class` varchar(12) NOT NULL,
   `Branch` varchar(12) NOT NULL,
-  `Year` varchar(12) NOT NULL,
+  `Year` int(10) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `institute` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unconfirmed_signup`
+--
+
+INSERT INTO `unconfirmed_signup` (`Name`, `Enrollment`, `Class`, `Branch`, `Year`, `Password`, `email`, `institute`) VALUES
+('', '', '', 'Branch', 20, '$2y$10$VGaqjD47DQux6t9h0/YtCOpuA7OtSGP1ZOhbWC/xuxEb88pTf0Nwa', '', 'Acropolis Institute of Technology and Research'),
+('', '', '', 'Branch', 20, '$2y$10$PvOGtEcJXI62hlo9DTf10eRsIhhyeb.vTtLOBv/zcpRDdHSiTIRzK', '', 'Acropolis Institute of Technology and Research');
 
 -- --------------------------------------------------------
 
@@ -100,31 +111,14 @@ CREATE TABLE `unconfirmed_signup` (
 --
 
 CREATE TABLE `uploads` (
-  `Enrollment` varchar(255) NOT NULL,
+  `user_id` int(10) NOT NULL,
   `fileaddress` varchar(255) NOT NULL,
-  `upload_id` int(20) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `time` time(6) NOT NULL,
+  `upload_id` int(10) NOT NULL,
+  `createdon` datetime NOT NULL,
   `likes` int(20) NOT NULL DEFAULT '0',
-  `dislikes` int(20) NOT NULL DEFAULT '0',
-  `share` int(20) NOT NULL DEFAULT '0'
+  `share` int(20) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `uploads`
---
-
-INSERT INTO `uploads` (`Enrollment`, `fileaddress`, `upload_id`, `description`, `date`, `time`, `likes`, `dislikes`, `share`) VALUES
-('0827IT171036', '0827IT171036-Success.png', 1, '', '0000-00-00', '00:00:00.000000', 0, 0, 0),
-('0827IT171036', '0827IT171036-Dontcompromise.png', 3, '', '0000-00-00', '00:00:00.000000', 0, 0, 0),
-('0827IT171036', '0827IT171036-pressure.png', 4, '', '0000-00-00', '00:00:00.000000', 0, 1, 0),
-('0827IT171019', '0827IT171019-Never_hurt.png', 5, '', '0000-00-00', '00:00:00.000000', 1, 0, 0),
-(' 0827IT17103', '_0827IT171034-news.png', 7, '', '0000-00-00', '00:00:00.000000', 0, 0, 0),
-('0827IT171033', '0827IT171033-news.png', 8, '', '0000-00-00', '00:00:00.000000', 7, 1, 0),
-('0827IT171036', '0827IT171036-Capture.PNG', 10, '', '0000-00-00', '00:00:00.000000', 0, 0, 0),
-('0827IT171036', '0827IT171036-imagecrousel.PNG', 11, '', '0000-00-00', '00:00:00.000000', 0, 0, 0),
-('0827IT171036', '0827IT171036-work_like.png', 13, '', '0000-00-00', '00:00:00.000000', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -133,27 +127,24 @@ INSERT INTO `uploads` (`Enrollment`, `fileaddress`, `upload_id`, `description`, 
 --
 
 CREATE TABLE `users` (
-  `Name` varchar(255) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
   `Enrollment` varchar(20) NOT NULL,
   `Class` varchar(12) NOT NULL,
-  `Branch` varchar(12) NOT NULL,
-  `Year` varchar(12) NOT NULL,
+  `Branch` varchar(50) NOT NULL,
+  `Year` int(10) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `institute` varchar(255) NOT NULL,
-  `profilepic` varchar(255) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `institute` varchar(100) NOT NULL,
+  `profilepic` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Name`, `Enrollment`, `Class`, `Branch`, `Year`, `Password`, `email`, `institute`, `profilepic`) VALUES
-('Hritik karma', ' 0827IT171034', 'IT-1', 'IT', '2017', '$2y$10$dHRcntGI80T0TRdVe/ae9e1vGeNK.LTkz5hjZ9YWlpFXGD0UjLXhq', 'keshavthegreat.ks@gmail.com', '', ''),
-('Astha jain ', '0827IT171019', 'IT-1', 'IT', '2017', '$2y$10$ILDNmD/okITymAfCIQdygOS8aitA4k2VStsld7Tdl.GBa8k2udBuO', 'keshavsuman96@gmail.com', '', ''),
-('Harshit pathak', '0827IT171032', 'IT', 'it', '2017', '$2y$10$PStDtQtNiaYe4Ao/M5LJ4uodQpL/oSG/nF.lpzbR6AlhR3Bn6Mwfy', 'keshavsuman96@gmail.com', '', ''),
-('keshavsuman96', '0827IT171033', 'IT-1', 'IT', '2017', '$2y$10$Es8oQheQvVO8wr2ojQtCVOwH4sLpaYaSj63y9EGhvfuhs19OTDOxG', 'keshavsuman96@gmail.com', 'Acropolis Institute of Technology And Research', ''),
-('keshav suman ', '0827IT171036', 'IT-1', 'IT', '2017', '$2y$10$rNG.mtZYcyq38t54Glw4leKUv7QtHi09LxZ0FoLIBeV8yjPar7B4O', 'keshavsuman96@gmail.com', '', '');
+INSERT INTO `users` (`user_id`, `Name`, `Enrollment`, `Class`, `Branch`, `Year`, `Password`, `email`, `institute`, `profilepic`) VALUES
+(6, 'keshav suman ', '0827IT171036', 'IT-1', 'IT', 2017, '$2y$10$rNG.mtZYcyq38t54Glw4leKUv7QtHi09LxZ0FoLIBeV8yjPar7B4O', 'keshavsuman96@gmail.com', '', '');
 
 -- --------------------------------------------------------
 
@@ -162,11 +153,10 @@ INSERT INTO `users` (`Name`, `Enrollment`, `Class`, `Branch`, `Year`, `Password`
 --
 
 CREATE TABLE `waiting` (
-  `Enrollment` varchar(12) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `enrollment` varchar(20) NOT NULL,
   `fileaddress` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `time` time(6) NOT NULL,
-  `upload_id` varchar(20) NOT NULL
+  `upload_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -189,7 +179,29 @@ ALTER TABLE `uploads`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`Enrollment`);
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `uploads`
+--
+ALTER TABLE `uploads`
+  MODIFY `upload_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
